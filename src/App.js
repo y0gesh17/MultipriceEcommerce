8db8c5209451e604f964c90f41a79f9f44a8c5c4
y0gesh17 <yogeshkadam8426@gmail.com>
2024-02-15 22:09:53 +0530

    client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navigation from './customer/Components/Navbar/Navigation';
+import CustomerRoutes from './Routers/CustomerRoutes';
+import AdminRoutes from './Routers/AdminRoutes';
+import NotFound from './Pages/Notfound';
+import AdminPannel from './Admin/AdminPannel';
+// import Routers from './Routers/Routers';
 
 function App() {
+  const isAdmin=true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      
+      <Routes>
+        <Route path="/*" element={<CustomerRoutes />} />
+        <Route path="/admin/*" element={<AdminPannel />} />
+        
+      </Routes>
     </div>
   );
 }
